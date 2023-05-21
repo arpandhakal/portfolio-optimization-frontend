b6847b579d7a6d6fe05b1cd2f97b81ab5573a116
arpandhakal <arpandhakal20@gmail.com>
2023-05-21 10:41:51 +0545

    add : redux initialization

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,135 @@
-import React from "react";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../action/authActions";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
-  return <div>Login</div>;
+function Login(props) {
+  const [errors, setErrors] = React.useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    const userData = {
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+    props.loginUser(userData);
+  };
+
+  React.useEffect(() => {
+    if (props.auth.isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [props.auth.isAuthenticated, props.history]);
+
+  React.useEffect(() => {
+    if (props.errors) {
+      setErrors(props.errors);
+    }
+  }, [props.errors]);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          {props.errors.errors && (
+            <div style={{ color: "red" }}>
+              {props.errors.errors.map((error, index) => (
+                <p key={index}>{error.user}</p>
+              ))}
+            </div>
+          )}
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+export default connect(mapStateToProps, { loginUser })(Login);
